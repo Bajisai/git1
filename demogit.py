83616b2e2a5ec9df038618f548e2dd6679d46f8d
name <bajisai77@gmail.com>
2025-03-16 19:38:36 +0530

    new commit

---
+++ demogit.py
@@ -0,0 +1,3 @@
+a = input(int("eneter age "))
+b = input(int("eneter name"))
+print(a+b)
--- g.py
+++ g.py
@@ -1,2 +1,11 @@
-a = 10
-b = 20
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="author" content="CodePel">
+      <title> CSS Blossoming Flowers at Magical Night Example </title>
+      <!-- Style CSS -->
+      <link rel="stylesheet" href="./css/style.css">
+      <!-- Demo CSS (No need to include it into your project) -->
+      <link rel="stylesheet" href="./css/demo.css">
